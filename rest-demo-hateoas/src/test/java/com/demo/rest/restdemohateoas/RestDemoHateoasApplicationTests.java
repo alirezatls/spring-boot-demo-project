f78caa542356f3ded0084beb9b51fa3efe6d17be
alireza <alirezatl135@gmail.com>
2019-02-03 09:28:43 -0800

    first commit add all project

---
+++ rest-demo-hateoas/src/test/java/com/demo/rest/restdemohateoas/RestDemoHateoasApplicationTests.java
@@ -0,0 +1,17 @@
+package com.demo.rest.restdemohateoas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestDemoHateoasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
---
+++ web-demo-thymeleaf-mail/src/main/java/com/demo/web/webdemothymeleafmail/WebDemoThymeleafMailApplication.java
@@ -0,0 +1,14 @@
+package com.demo.web.webdemothymeleafmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDemoThymeleafMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDemoThymeleafMailApplication.class, args);
+	}
+
+}
+
